a9e5815f0b5f5318e81a8e144a6d67fb2cdd1084
Alexander-Ottaway <alexander.ottaway@microsoft.wsei.edu.pl>
2024-11-26 14:55:04 +0100

    Lab1 - zrobione na zajeciach

---
+++ WebApp2/Controllers/MovieApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp2.Models.Movies;
+
+namespace WebApp2.Controllers;
+
+[ApiController]
+[Route("/api/companies")]
+public class MovieApiController: ControllerBase
+{
+    private readonly MoviesDbContext _context;
+
+    public MovieApiController(MoviesDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetFiltered(string fragment)
+    {
+        return Ok(
+            _context.ProductionCompanies.Where(c => c.CompanyName != null && c.CompanyName.Contains(fragment))
+                .AsNoTracking()
+                .AsAsyncEnumerable()
+        );
+    }
+}
